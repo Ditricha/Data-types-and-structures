36273ca43db74bf144bd298e020db3eb5619fd4c
Ditricha <51129541+Ditricha@users.noreply.github.com>
2020-11-30 17:20:10 +0300

    Update HuffCode.h

--- Lab_5.3/HuffCode.h
+++ Lab_5.3/HuffCode.h
@@ -4,99 +4,86 @@
 
 using namespace std;
 
-typedef map<char, string> HuffCodeMap;
+typedef map<char, string> huff_code_map;
 class HuffCode
 {
 public:
 	HuffCode();
-	HuffCodeMap codes;
-	void decodeFromRoot(const INode* root, string encodedString, string &decodedString);
-	void GenerateCodes(const INode* node, const string code);
+	huff_code_map codes;
+	void decode_from_root(const INode* root, string encoded_string, string &decoded_string);
+	void generate_codes(const INode* node, const string code);
 	string encode(ifstream& input);
 private:
-	void decode(const INode* root, const INode* node, string encodedString, string &decodedString);
+	void decode(const INode* root, const INode* node, string encoded_string, string &decoded_string);
 };
 
 HuffCode::HuffCode()
 {
 }
 
-void HuffCode::GenerateCodes(const INode* node, const string code)
+void HuffCode::generate_codes(const INode* node, const string code)
 {
-	if (const LeafNode* lf = dynamic_cast<const LeafNode*>(node))
-	{
+	if (const LeafNode* lf = dynamic_cast<const LeafNode*>(node)) {
 		codes[lf->c] = code;
 		cout << code << " - " << lf->c << endl;
 	}
-	else if (const InternalNode* in = dynamic_cast<const InternalNode*>(node))
-	{
-		string leftCode = code;
-		leftCode.append("0");
-		GenerateCodes(in->left, leftCode);
+	else if (const InternalNode* in = dynamic_cast<const InternalNode*>(node)) {
+		string left_Code = code;
+		left_Code.append("0");
+		generate_codes(in->left, left_Code);
 
-		string rightCode = code;
-		rightCode.append("1");
-		GenerateCodes(in->right, rightCode);
+		string right_Code = code;
+		right_Code.append("1");
+		generate_codes(in->right, right_Code);
 	}
 }
 
 string HuffCode::encode(ifstream& input)
 {
 	map<char, string>::iterator i_code = codes.begin();
-	string encodedString;
+	string encoded_string;
 	char c;
 
-	while (input.get(c))
-	{
+	while (input.get(c)) {
 		i_code = codes.find(c);
-		encodedString.append(i_code->second);
+		encoded_string.append(i_code->second);
 	}
-
 	i_code = codes.find('^');
-	encodedString.append(i_code->second);
-	return encodedString;
+	encoded_string.append(i_code->second);
+	return encoded_string;
 }
 
 
-void HuffCode::decodeFromRoot(const INode* root, string encodedString, string &decodedString)
+void HuffCode::decode_from_root(const INode* root, string encoded_string, string &decoded_string)
 {
 	const INode* node = root;
-	if (const InternalNode* in = dynamic_cast<const InternalNode*>(node))
-	{
-		if (encodedString[0] == '0')
-		{
-			encodedString.erase(encodedString.begin());
-			decode(root, in->left, encodedString, decodedString);
+	if (const InternalNode* in = dynamic_cast<const InternalNode*>(node)) {
+		if (encoded_string[0] == '0') {
+			encoded_string.erase(encoded_string.begin());
+			decode(root, in->left, encoded_string, decoded_string);
 		}
-		else
-		{
-			encodedString.erase(encodedString.begin());
-			decode(root, in->right, encodedString, decodedString);
+		else {
+			encoded_string.erase(encoded_string.begin());
+			decode(root, in->right, encoded_string, decoded_string);
 		}
 	}
 }
-void HuffCode::decode(const INode* root, const INode* node, string encodedString, string &decodedString)
+void HuffCode::decode(const INode* root, const INode* node, string encoded_string, string &decoded_string)
 {
-	if (const LeafNode* lf = dynamic_cast<const LeafNode*>(node))
-	{
-		if (lf->c != '^')
-		{
-			decodedString += lf->c;
-			decodeFromRoot(root, encodedString, decodedString);
+	if (const LeafNode* lf = dynamic_cast<const LeafNode*>(node)) {
+		if (lf->c != '^') {
+			decoded_string += lf->c;
+			decode_from_root(root, encoded_string, decoded_string);
 		}
-
 	}
-	else if (const InternalNode* in = dynamic_cast<const InternalNode*>(node))
-	{
-		if (encodedString[0] == '0')
-		{
-			encodedString.erase(encodedString.begin());
-			decode(root, in->left, encodedString, decodedString);
+	else if (const InternalNode* in = dynamic_cast<const InternalNode*>(node)) {
+		if (encoded_string[0] == '0') {
+			encoded_string.erase(encoded_string.begin());
+			decode(root, in->left, encoded_string, decoded_string);
 		}
-		else
-		{
-			encodedString.erase(encodedString.begin());
-			decode(root, in->right, encodedString, decodedString);
+		else {
+			encoded_string.erase(encoded_string.begin());
+			decode(root, in->right, encoded_string, decoded_string);
 		}
 	}
 }
